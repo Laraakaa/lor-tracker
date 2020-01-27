9bb6dc927b3473ddec5a77ea966d5ca0afbfea77
Lars Bärtschi <lars.baertschi@bluewin.ch>
2020-01-27 18:57:40 +0100

    initial version of tracker frontend

---
+++ LoRTracker/LoRTracker/LoRConnector.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace LoRTracker
+{
+    class LoRConnector
+    {
+        private int _port;
+        private Thread _thread;
+        private ClientConnectionStatus _status = ClientConnectionStatus.UNKNOWN;
+
+        public LoRConnector(int port)
+        {
+            _port = port;
+
+            _thread = new Thread(RunThread);
+        }
+
+        public void Connect()
+        {
+            _thread.Start();
+        }
+
+        private void RunThread()
+        {
+            while(true)
+            {
+                Tick();
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void Tick()
+        {
+            if (_status == ClientConnectionStatus.UNKNOWN)
+            {
+                // initial connect
+                OnStateChanged(new StateChangedEventArgs { NextState = ClientConnectionStatus.CONNECTING });
+            }
+        }
+
+        protected virtual void OnStateChanged(StateChangedEventArgs e)
+        {
+            _status = e.NextState;
+            StateChanged?.Invoke(this, e);
+        }
+
+        public event StateChangedEventHandler StateChanged;
+        public delegate void StateChangedEventHandler(LoRConnector sender, StateChangedEventArgs e);
+    }
+
+    public class StateChangedEventArgs : EventArgs
+    {
+        public ClientConnectionStatus NextState { get; set; }
+    }
+
+    public enum ClientConnectionStatus
+    {
+        UNKNOWN,
+        CONNECTING,
+        CONNECTED,
+        DISCONNECTED
+    }
+}
---
+++ LoRTracker/LoRTracker/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LoRTracker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        LoRConnector ClientConnector;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ClientConnector = new LoRConnector(21337);
+            ClientConnector.StateChanged += ClientStatusChanged;
+
+            ClientConnector.Connect();
+        }
+
+        void ClientStatusChanged(LoRConnector sender, StateChangedEventArgs e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                lblGameConnectionStatus.Content = "Client: " + e.NextState.ToString();
+
+                switch (e.NextState)
+                {
+                    case ClientConnectionStatus.CONNECTED:
+                        lblGameConnectionStatus.Foreground = new SolidColorBrush(Colors.Green);
+                        break;
+                    case ClientConnectionStatus.CONNECTING:
+                        lblGameConnectionStatus.Foreground = new SolidColorBrush(Colors.DarkOrange);
+                        break;
+                    case ClientConnectionStatus.DISCONNECTED:
+                        lblGameConnectionStatus.Foreground = new SolidColorBrush(Colors.Red);
+                        break;
+                    case ClientConnectionStatus.UNKNOWN:
+                        lblGameConnectionStatus.Foreground = new SolidColorBrush(Colors.Black);
+                        break;
+                }
+            });
+        }
+    }
+}
